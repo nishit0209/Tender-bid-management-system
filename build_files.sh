d7584027ee6de20c01c90a2306d73796d5a89502
nishit0209 <nishitbhavsar296@gmail.com>
2026-06-17 14:50:03 +0530

    update vercel file

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo " BUILD START"
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
-python3.9 manage.py migrate
+python3.12 -m pip install -r requirements.txt
+python3.12 manage.py collectstatic --noinput --clear
+python3.12 manage.py migrate
 echo " BUILD END"
